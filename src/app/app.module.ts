0bc252ac7e967c2dbebbf41d77d5847a5ecca8ef
fawwaz <fawwazuddin.m@prodapt.com>
2023-09-20 17:38:46 +0530

    student pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component'
+
+
+import { StudentComponent } from './student/student.component';
+import { AgePipe } from './age.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    StudentComponent,
+    AppComponent,
+    AgePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:[AppComponent]
 })
 export class AppModule { }
